23f04aba354e694a7cb35d9a36cb9ce1252c8a32
feeelibread <feeelpoos@gmail.com>
2022-03-23 15:10:27 -0300

    Aprendendo sobre como os caracteres funcionam e também sobre as atribuições de variáveis

--- 5-CaracteresETextos/Program.cs
+++ 5-CaracteresETextos/Program.cs
@@ -6,7 +6,29 @@ namespace _5_CaracteresETextos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            //Character
+            char primeiraLetra = 'ã';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            //Strings
+            string titulo = "Alura curso online de tecnlogia, descobri pelo gavetaaaa " + 2022;
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = @" - .NET
+ - Java 
+ - Javascript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
         }
     }
 }
---
+++ 6-AtribuiçõesDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_AtribuiçõesDeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6 - Atribuições de variáveis");
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
+        }
+    }
+}
