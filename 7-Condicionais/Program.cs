c71a1b6fe2f63b71d94ceb3e985bc63bff14863b
feeelibread <feeelpoos@gmail.com>
2022-03-24 10:44:24 -0300

    Aprendendo sobre condicionais If e Else, e também operadores lógicos como OU (representado no C# pelo pipe ||) ou E (representado no C# pelo &&)

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7 - Condicionais");
+            int idade = 16;
+            int quantidadeDePessoas = 2;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("João possuí 18 anos, portanto, pode entrar.");
+            }
+            else
+            {
+                if (quantidadeDePessoas >= 2)
+                {
+                    Console.WriteLine("João não possuí 18 anos, mas, está acompanhado e portanto, pode entrar.");
+                }
+                else
+                {
+                    Console.WriteLine("João não possuí 18 anos, não pode entrar.");
+                }
+            }
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+            int idade = 16;
+            int quantidadeDePessoas = 2;
+
+            if (idade >= 18 || quantidadeDePessoas >= 2)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+
+            }
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
+        }
+    }
+}
