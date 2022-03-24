3fc2d7b7439867d57041d2d848d5615d86398f1c
feeelibread <feeelpoos@gmail.com>
2022-03-24 11:22:39 -0300

    Aprendendo sobre o uso do tipo de variável condicional chamada Bool

--- 8-Condicionais2/Program.cs
+++ 8-Condicionais2/Program.cs
@@ -8,9 +8,13 @@ namespace _8_Condicionais2
         {
             Console.WriteLine("Executando o projeto 8 - Condicionais 2");
             int idade = 16;
-            int quantidadeDePessoas = 2;
 
-            if (idade >= 18 || quantidadeDePessoas >= 2)
+            //int quantidadeDePessoas = 2;
+            //bool acompanhado = quantidadeDePessoas >= 2;
+
+            bool acompanhado = false;
+
+            if (idade >= 18 && acompanhado == true)
             {
                 Console.WriteLine("Pode entrar.");
             }
