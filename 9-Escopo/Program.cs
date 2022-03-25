fd4851eb267a93f8e0d6030e3b1b6dc4e2bb56fc
feeelibread <feeelpoos@gmail.com>
2022-03-25 11:52:30 -0300

    Adicionando uma mensagem adicional no escopo do If

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 9 - Escopo");
+            int idade = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+
+            if (idade >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+
+            }
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
+        }
+    }
+}
