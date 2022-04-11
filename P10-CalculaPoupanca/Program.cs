5e109970b29bccf0f49b0c4ead582c3ba7156aa0
feeelibread <feeelpoos@gmail.com>
2022-04-11 10:39:27 -0300

    Criando um código que calcula poupança, por enquanto não implmenetando o while.

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P10_CalculaPoupanca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000.0;
+
+            // 0.036% = 0.0036
+            valorInvestido += valorInvestido * 0.0036;
+            Console.WriteLine($"Após 1 mês, você terá R${valorInvestido}");
+
+            valorInvestido += valorInvestido * 0.0036;
+            Console.WriteLine($"Após 2 mese, você terá R${valorInvestido}");
+
+
+            Console.WriteLine("Finalizando o projeto, aperte enter. . .");
+            Console.ReadLine();
+
+        }
+    }
+}
