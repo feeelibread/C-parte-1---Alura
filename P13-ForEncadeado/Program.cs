aadbc7bafb57ee2e99a37ed893bbaf525c315316
feeelibread <feeelpoos@gmail.com>
2022-04-14 16:35:04 -0300

    Duas formas de encadear os blocos de For para imprimir os asteriscos

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13 - For Encadeado");
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+            //********
+            //*********
+
+            //Imprimindo os asteriscos usando o BREAK
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //Uma forma diferente 
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
