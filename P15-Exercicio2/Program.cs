046c6fc68d57ad3678568ee68c32aed7bf235ec1
feeelibread <feeelpoos@gmail.com>
2022-04-14 17:00:41 -0300

    Exercicios envolvendo o For

---
+++ P15-Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P15_Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                fatorial *= i;
+                Console.WriteLine($"O fatorial de {i}, é = {fatorial}");
+            }
+        }
+    }
+}
