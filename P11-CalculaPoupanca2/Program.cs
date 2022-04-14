3017b58b52763556a46cf0e16d3a0cc704973a88
feeelibread <feeelpoos@gmail.com>
2022-04-14 16:03:54 -0300

    Calculando poupança utilizando o For

---
+++ P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P11_CalculaPoupanca2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11");
+            double valorInvestido = 1000.0;
+            
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine($"Após o mês {contadorMes}, você terá R${valorInvestido} investido");
+            }
+
+        }
+    }
+}
